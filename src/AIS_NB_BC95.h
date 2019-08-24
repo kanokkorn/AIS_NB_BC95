ef0d0c63362a0aacc2e58a6929732718b119987e
kanokkorn <kanokorn@outlook.jp>
2019-08-24 09:25:06 +0700

    Format code

--- src/AIS_NB_BC95.h
+++ src/AIS_NB_BC95.h
@@ -3,6 +3,7 @@
 
 #define ATLSOFTSERIAL 1
 
+#include <avr.h>
 #include <Arduino.h>
 #include <Stream.h>
 
@@ -13,30 +14,26 @@
 	#include "AltSoftSerial.h"
 #endif
 
-struct AIS_NB_BC95_RES
-{
+struct AIS_NB_BC95_RES{
 	unsigned char status;
 	String data;
 	String temp;
 };
 
-struct signal
-{
+struct signal{
 	String csq;
 	String rssi;
 	String ber;
 };
 
-struct UDPSend
-{
+struct UDPSend{
 	bool status;
 	String strsend;
 	unsigned char socket;
 	unsigned int length;
 };
 
-struct UDPReceive
-{
+struct UDPReceive{
 	unsigned char socket;
 	String ip_address;
 	unsigned int port;
@@ -44,72 +41,70 @@ struct UDPReceive
 	String data;
 	unsigned int remaining_length;
 };
-struct pingRESP
-{
+
+struct pingRESP{
 	bool status;
 	String addr;
 	String ttl;
 	String rtt;
 };
-class AIS_NB_BC95
-{
-public:
-	AIS_NB_BC95();
-	bool debug;
 
-	void (*Event_debug)(char *data);
-	void reset();
-	void rebootModule();
-	bool waitReady();
-	bool setPhoneFunction(unsigned char mode);
+class AIS_NB_BC95{
+  public:
+	  AIS_NB_BC95();
+	  bool debug;
 
-	//General
-	String getFirmwareVersion();
-	String getIMEI();
+	  void (*Event_debug)(char *data);
+	  void reset();
+	  void rebootModule();
+	  bool waitReady();
+	  bool setPhoneFunction(unsigned char mode);
+
+	  //General
+	  String getFirmwareVersion();
+	  String getIMEI();
     String getIMSI();
-	pingRESP pingIP(String IP);
+	  pingRESP pingIP(String IP);
 
-	//Network
-	bool setAutoConnectOn();
-	bool setAutoConnectOff();
-	String getAutoConnect();
-	String getNetworkStatus();
+	  //Network
+	  bool setAutoConnectOn();
+	  bool setAutoConnectOff();
+	  String getAutoConnect();
+	  String getNetworkStatus();
     //bool setAPN(String apn);
-	String getAPN();
+	  String getAPN();
 
-
-	bool cgatt(unsigned char mode);
-	bool getNBConnect();
+	  bool cgatt(unsigned char mode);
+	  bool getNBConnect();
 	
-	void setupDevice(String serverPort);
-	bool attachNB(String serverPort);
-	bool detachNB();
+	  void setupDevice(String serverPort);
+	  bool attachNB(String serverPort);
+	  bool detachNB();
 
-	String getDeviceIP();
-	signal getSignal();
+	  String getDeviceIP();
+	  signal getSignal();
 
-	void createUDPSocket(String port);
+	  void createUDPSocket(String port);
 
     UDPSend sendUDPmsg(String addressI,String port,String data);
-	UDPSend sendUDPmsg(String addressI,String port,unsigned int len,char *data,unsigned char send_mode);
-	UDPSend sendUDPmsgStr(String addressI,String port,String data);
+	  UDPSend sendUDPmsg(String addressI,String port,unsigned int len,char *data,unsigned char send_mode);
+	  UDPSend sendUDPmsgStr(String addressI,String port,String data);
 	
-	bool closeUDPSocket();
+	  bool closeUDPSocket();
 
-	UDPReceive waitResponse();
+	  UDPReceive waitResponse();
 
-	void printHEX(char *str);
-	String toString(String dat);
-	String str2HexStr(String strin);
-	char char_to_byte(char c);
+	  void printHEX(char *str);
+	  String toString(String dat);
+	  String str2HexStr(String strin);
+	  char char_to_byte(char c);
 	
-	void receive_UDP(UDPReceive rx);
+	  void receive_UDP(UDPReceive rx);
 
-private:
+  private:
 		AIS_NB_BC95_RES wait_rx_bc(long tout,String str_wait);
 
-protected:
-	 Stream *_Serial;	
+  protected:
+	  Stream *_Serial;	
 };
-
 #endif
